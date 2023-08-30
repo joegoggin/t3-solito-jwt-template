dda1e20c1863f9ff27b309e291ad97c83548dca9
joegoggin <joegoggin27@gmail.com>
2023-08-30 17:33:42 -0400

    add Sign In logic

--- packages/app/screens/auth/SignInScreen.tsx
+++ packages/app/screens/auth/SignInScreen.tsx
@@ -2,28 +2,70 @@ import { Card, Layout } from "app/ui/layout";
 import { View } from "app/ui/view";
 import { InputLabel, TextInput } from "app/ui/inputs";
 import { H1 } from "app/ui/typography";
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "app/ui/buttons";
+import { handleChangeText } from "app/utils/handleChangeText";
+import { api } from "app/utils/trpc";
+import { useRouter } from "solito/router";
 
-type SignInScreenProps = {};
+const SignInScreen: React.FC = () => {
+    // state
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
 
-const SignInScreen: React.FC<SignInScreenProps> = () => {
+    // mutations
+    const signIn = api.auth.signIn.useMutation();
+
+    // router
+    const router = useRouter();
+
+    // event handlers
+    const handleSignUpNavigation = () => {
+        router.push("/sign-up");
+    };
+
+    const handleSignIn = async () => {
+        try {
+            const userData = await signIn.mutateAsync({
+                email: email.toLowerCase(),
+                password,
+            });
+
+            if (userData) {
+                console.log(userData);
+                setEmail("");
+                setPassword("");
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
         <Layout>
             <Card>
                 <H1 className="text-black">Sign In</H1>
                 <View className="mt-10 w-[80%]">
-                    <InputLabel>Username</InputLabel>
-                    <TextInput className="mb-2" />
+                    <InputLabel>Email</InputLabel>
+                    <TextInput
+                        className="mb-2"
+                        value={email}
+                        onChangeText={handleChangeText(setEmail)}
+                    />
+
                     <InputLabel>Password</InputLabel>
-                    <TextInput />
+                    <TextInput
+                        value={password}
+                        secureTextEntry={true}
+                        onChangeText={handleChangeText(setPassword)}
+                    />
                 </View>
                 <View className="mt-20 w-[80%] flex-row justify-evenly">
-                    <Button text="Sign In" onPress={() => { }} />
+                    <Button text="Sign In" onPress={handleSignIn} />
                     <Button
                         className="bg-black"
                         text="Sign Up"
-                        onPress={() => { }}
+                        onPress={handleSignUpNavigation}
                     />
                 </View>
             </Card>
