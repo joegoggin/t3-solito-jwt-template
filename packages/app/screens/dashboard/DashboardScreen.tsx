067e6203bf23791f71c87c6f3e1d4cb7c17efac5
joegoggin <joegoggin27@gmail.com>
2023-09-04 17:09:57 -0400

    add Sign Out button to dashboard

--- packages/app/screens/dashboard/DashboardScreen.tsx
+++ packages/app/screens/dashboard/DashboardScreen.tsx
@@ -2,13 +2,14 @@ import { useUser } from "app/provider/context/UserContextProvider";
 import { Button } from "app/ui/buttons";
 import { Layout } from "app/ui/layout";
 import { H1 } from "app/ui/typography";
+import { View } from "app/ui/view";
 import React from "react";
 import { Roles } from "server/models/enums/Role";
 import { useRouter } from "solito/router";
 
 const UserDashboardScreen: React.FC = () => {
     // context
-    const { user, role } = useUser();
+    const { user, role, clearUserData } = useUser();
 
     // router
     const router = useRouter();
@@ -18,17 +19,30 @@ const UserDashboardScreen: React.FC = () => {
         router.push("/admin-only");
     };
 
+    const handleSignOut = () => {
+        clearUserData();
+        router.push("/sign-in");
+    };
+
     return (
         <Layout>
             <H1 className="text-white">Hello {user?.fName}!</H1>
 
-            {role && role === Roles.Admin && (
+            <View className="mt-10 w-[40%] flex-row items-center justify-evenly">
                 <Button
-                    className="mt-10 bg-black text-white"
-                    text="Admin Only"
-                    onPress={handleAdminOnlyNavigation}
+                    className="bg-black text-white"
+                    text="Sign Out"
+                    onPress={handleSignOut}
                 />
-            )}
+
+                {role && role === Roles.Admin && (
+                    <Button
+                        className="bg-white text-black"
+                        text="Admin Only"
+                        onPress={handleAdminOnlyNavigation}
+                    />
+                )}
+            </View>
         </Layout>
     );
 };
